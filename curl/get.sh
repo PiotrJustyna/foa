aaac0409759d280ea3bbc27b0d572b0dc272c726
Piotr Justyna <piotr.justyna@gmail.com>
2018-10-14 23:49:48 +0100

    Added curls for existing operations.

---
+++ curl/get.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl http://localhost:3000/feedback?id=1e9fe6eb-1b91-4180-97d5-68dda1ce4843
--- curl/post.sh
+++ curl/post.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -o -H "Content-Type: application/json" -X POST "http://localhost:3000/feedback" -d "{key1:value1, key2:value2}" 
+curl -H "Content-Type: application/json" -X POST http://localhost:3000/feedback -d '{"key1":"value1", "key2":"value2"}'
